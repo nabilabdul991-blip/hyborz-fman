601dbed9a4298cd52afa02bba107661238a86a6b
nabilabdul991-blip <nabilabdul991@gmail.com>
2026-05-17 04:52:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
-const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
+const { default: makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion } =
+require("@whiskeysockets/baileys");
+
+const readline = require("readline");
+
+function question(text) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    return new Promise(resolve => {
+        rl.question(text, answer => {
+            rl.close();
+            resolve(answer);
+        });
+    });
+}
 
 async function startBot() {
 
-    // 🔐 SESSION AUTH (INI YANG KURANG DI KAMU)
     const { state, saveCreds } = await useMultiFileAuthState("./session");
+    const { version } = await fetchLatestBaileysVersion();
 
     const sock = makeWASocket({
         auth: state,
-        printQRInTerminal: true
+        version
     });
 
-    // simpan session otomatis
     sock.ev.on("creds.update", saveCreds);
 
-    console.log("🤖 Bot jalan... scan QR kalau diminta");
+    // ================= PAIRING CODE =================
+    if (!sock.authState.creds.registered) {
+
+        const phoneNumber = await question("📱 Masukkan nomor (62xxxx): ");
+
+        const code = await sock.requestPairingCode(phoneNumber.trim());
+
+        console.log("\n🔑 PAIRING CODE ANDA:");
+        console.log(code);
+        console.log("\n👉 Masukkan kode ini di WhatsApp (Linked Devices)");
+    }
+
+    // ================= CONNECT STATUS =================
+    sock.ev.on("connection.update", (update) => {
+        const { connection } = update;
+
+        if (connection === "open") {
+            console.log("🤖 Bot berhasil connect!");
+        }
+    });
 
     // ================= MESSAGE =================
     sock.ev.on("messages.upsert", async (m) => {
@@ -27,7 +62,7 @@ async function startBot() {
             "";
 
         if (text === "menu") {
-            await sock.sendMessage(from, { text: "✅ Bot sudah FIX dan jalan" });
+            await sock.sendMessage(from, { text: "✅ Bot aktif (pairing mode)" });
         }
     });
 }
