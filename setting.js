a2355bd735da9ecd4bc89ab70c3c3aceb6f4229b
nabilabdul991-blip <nabilabdul991@gmail.com>
2026-05-17 03:24:52 +0700

    setting.js

---
+++ setting.js
@@ -0,0 +1,31 @@
+// settings.js
+// Konfigurasi dasar bot
+
+module.exports = {
+    // Nama bot
+    botName: "MIRZKATIX",
+
+    // Owner / pemilik bot
+    ownerName: "Firman",
+
+    // Nomor owner (pakai format internasional tanpa +)
+    ownerNumber: "6287825121119",
+
+    // Prefix command bot
+    prefix: ".",
+
+    // Mode bot
+    publicMode: true,
+
+    // Auto reply aktif/tidak
+    autoReply: true,
+
+    // Pesan welcome grup
+    welcomeMessage: "Selamat datang di grup!",
+
+    // Pesan goodbye grup
+    goodbyeMessage: "Selamat tinggal!",
+
+    // Watermark
+    wm: "MyBot © 2026"
+};
