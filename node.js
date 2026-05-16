1ecde39cad6325aad7f5885504d322a1e7b48a3c
nabilabdul991-blip <nabilabdul991@gmail.com>
2026-05-17 03:31:17 +0700

    node.js

---
+++ node.js
@@ -0,0 +1,11 @@
+bot/
+ ├── index.js
+ ├── settings.js
+ ├── lib/
+ │    ├── antilink.js
+ │    ├── list.js
+ │    ├── owner.js
+ ├── plugins/
+ │    ├── downloader.js
+ │    ├── ai.js
+ │    ├── tools.js
